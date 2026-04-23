08a3913949d9ad482934f29ce0fe3b18def0baf3
Inksaver <Inksaver@users.noreply.github.com>
2026-04-23 16:57:43 +0100

    local version = 20260423.1600

--- lib/ui/Textbox.lua
+++ lib/ui/Textbox.lua
@@ -1,5 +1,4 @@
-local version = 20250913.0900
--- https://pastebin.com/awnymvaT lib.ui.Textbox.lua
+local version = 20260423.1600
 local Class = require("lib.Class")
 local T = Class:derive("Textbox")
 local Vector2 = require("lib.Vector2")
