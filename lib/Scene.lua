d161d1fe4eefc40d0fc93c842b7a6dc9727f284a
Inksaver <Inksaver@users.noreply.github.com>
2026-04-23 16:49:56 +0100

    local version = 20260423.1600

--- lib/Scene.lua
+++ lib/Scene.lua
@@ -1,6 +1,4 @@
-local version = 20250914.1600
--- pastebin(1): cTUeYqhF lib.Scene.lua
--- pastebin(2): eBMmftm4
+local version = 20260423.1600
 local Class = require("lib.Class")
 local EntityMgr = require("lib.EntityMgr")
 local Scene = Class:derive("Scene")
